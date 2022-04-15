201827fe6787a6b1816e0a3a2866868351da0f09
XiaoyangWang <2626395W@student.gla.ac.uk>
2022-04-15 18:13:58 +0100

    change frame

---
+++ fake_sensor_demo/demo_sensor_server.cpp
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2013-2021  Bernd Porr <mail@berndporr.me.uk>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License.
+ *
+ */
+
+#include <string.h>
+#include <unistd.h>
+
+#include "json_fastcgi_web_api.h"
+#include "fakesensor.h"
+
+/**
+ * Flag to indicate that we are running.
+ * Needed later to quit the idle loop.
+ **/
+int mainRunning = 1;
+
+/**
+ * Handler when the user has pressed ctrl-C
+ * send HUP via the kill command.
+ **/
+void sigHandler(int sig) { 
+	if((sig == SIGHUP) || (sig == SIGINT)) {
+		mainRunning = 0;
+	}
+}
+
+
+/** 
+ * Sets a signal handler so that you can kill
+ * the background process gracefully with:
+ * kill -HUP <PID>
+ **/
+void setHUPHandler() {
+	struct sigaction act;
+	memset (&act, 0, sizeof (act));
+	act.sa_handler = sigHandler;
+	if (sigaction (SIGHUP, &act, NULL) < 0) {
+		perror ("sigaction");
+		exit (-1);
+	}
+	if (sigaction (SIGINT, &act, NULL) < 0) {
+		perror ("sigaction");
+		exit (-1);
+	}
+}
+
+
+/**
+ * Handler which receives the data here just saves
+ * the most recent sample with timestamp. Obviously,
+ * in a real application the data would be stored
+ * in a database and/or triggers events and other things!
+ **/
+class SENSORfastcgicallback : public SensorCallback {
+public:
+	std::deque<cv::Mat> temperatureBuffer;
+	std::deque<long> timeBuffer;
+	long t;
+	const int maxBufSize = 50;
+
+	/**
+	 * Callback with the fresh ADC data.
+	 * That's where all the internal processing
+	 * of the data is happening. Here, we just
+	 * convert the raw ADC data to temperature
+	 * and store it in a variable.
+	 **/
+	virtual void hasSample(cv::Mat v) {
+		temperatureBuffer.push_back(v);
+		if (temperatureBuffer.size() > maxBufSize) temperatureBuffer.pop_front();
+		// timestamp
+		t = getTimeMS();
+		timeBuffer.push_back(t);
+		if (timeBuffer.size() > maxBufSize) timeBuffer.pop_front();
+	}
+
+	void forceTemperature(cv::Mat temp) {
+		for(auto& v:temperatureBuffer) {
+			v = temp;
+		}
+	}
+
+private:
+	static unsigned long getTimeMS() {
+                std::chrono::time_point<std::chrono::system_clock> now = 
+                        std::chrono::system_clock::now();
+                auto duration = now.time_since_epoch();
+                return (unsigned long)std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        }
+};
+
+
+/**
+ * Callback handler which returns data to the
+ * nginx server. Here, simply the current temperature
+ * and the timestamp is transmitted to nginx and the
+ * javascript application.
+ **/
+class JSONCGIADCCallback : public JSONCGIHandler::GETCallback {
+private:
+	/**
+	 * Pointer to the ADC event handler because it keeps
+	 * the data in this case. In a proper application
+	 * that would be probably a database class or a
+	 * controller keeping it all together.
+	 **/
+	SENSORfastcgicallback* sensorfastcgi;
+
+public:
+	/**
+	 * Constructor: argument is the ADC callback handler
+	 * which keeps the data as a simple example.
+	 **/
+	JSONCGIADCCallback(SENSORfastcgicallback* argSENSORfastcgi) {
+		sensorfastcgi = argSENSORfastcgi;
+	}
+
+	/**
+	 * Gets the data sends it to the webserver.
+	 * The callback creates two json entries. One with the
+	 * timestamp and one with the temperature from the sensor.
+	 **/
+	virtual std::string getJSONString() {
+		JSONCGIHandler::JSONGenerator jsonGenerator;
+		jsonGenerator.add("epoch",(long)time(NULL));
+		jsonGenerator.add("temperature",sensorfastcgi->temperatureBuffer);
+		jsonGenerator.add("time",sensorfastcgi->timeBuffer);
+		return jsonGenerator.getJSON();
+	}
+};
+
+
+/**
+ * Callback handler which receives the JSON from jQuery
+ **/
+class SENSORPOSTCallback : public JSONCGIHandler::POSTCallback {
+public:
+	SENSORPOSTCallback(SENSORfastcgicallback* argSENSORfastcgi) {
+		sensorfastcgi = argSENSORfastcgi;
+	}
+
+	/**
+	 * As a crude example we force the temperature readings
+	 * to be 20 degrees for a certain number of timesteps.
+	 **/
+	virtual void postString(std::string postArg) {
+		auto m = JSONCGIHandler::postDecoder(postArg);
+		float temp = atof(m["temperature"].c_str());
+		std::cerr << m["hello"] << "\n";
+		sensorfastcgi->forceTemperature(temp);
+	}
+
+	/**
+	 * Pointer to the handler which keeps the temperature
+	 **/
+	SENSORfastcgicallback* sensorfastcgi;
+};
+	
+
+// Main program
+int main(int argc, char *argv[]) {
+	// getting all the ADC related acquistion set up
+	FakeSensor* sensorcomm = new FakeSensor();
+	SENSORfastcgicallback sensorfastcgicallback;
+	sensorcomm->setCallback(&sensorfastcgicallback);
+
+	// Setting up the JSONCGI communication
+	// The callback which is called when fastCGI needs data
+	// gets a pointer to the SENSOR callback class which
+	// contains the samples. Remember this is just a simple
+	// example to have access to some data.
+	JSONCGIADCCallback fastCGIADCCallback(&sensorfastcgicallback);
+
+	// Callback handler for data which arrives from the the
+	// browser via jquery json post requests:
+	// $.post( 
+        //              "/sensor/:80",
+        //              {
+	//		  temperature: [20,18,19,20],
+	//                time: [171717,171718,171719,171720],
+	//                hello: "Hello, that's a test!"
+	//	      }
+	//	  );
+	SENSORPOSTCallback postCallback(&sensorfastcgicallback);
+	
+	// starting the fastCGI handler with the callback and the
+	// socket for nginx.
+	JSONCGIHandler* fastCGIHandler = new JSONCGIHandler(&fastCGIADCCallback,
+							    &postCallback,
+							    "/tmp/sensorsocket");
+
+	// starting the data acquisition at the given sampling rate
+	sensorcomm->startSensor();
+
+	// catching Ctrl-C or kill -HUP so that we can terminate properly
+	setHUPHandler();
+
+	fprintf(stderr,"'%s' up and running.\n",argv[0]);
+
+	// Just do nothing here and sleep. It's all dealt with in threads!
+	// At this point for example a GUI could be started such as QT
+	// Here, we just wait till the user presses ctrl-c which then
+	// sets mainRunning to zero.
+	while (mainRunning) sleep(1);
+
+	fprintf(stderr,"'%s' shutting down.\n",argv[0]);
+
+	// stopping ADC
+	delete sensorcomm;
+
+	// stops the fast CGI handlder
+	delete fastCGIHandler;
+	
+	return 0;
+}
---
+++ fake_sensor_demo/fakesensor.h
@@ -0,0 +1,104 @@
+#ifndef __FAKESENSOR_H
+#define __FAKESENSOR_H
+
+/**
+ * Copyright (c) 2021  Bernd Porr <mail@berndporr.me.uk>
+ **/
+
+#include <math.h>
+#include "CppTimer.h"
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+
+
+/**
+ * Callback for new samples which needs to be implemented by the main program.
+ * The function hasSample needs to be overloaded in the main program.
+ **/
+class SensorCallback {
+public:
+	/**
+	 * Called after a sample has arrived.
+	 **/
+	virtual void hasSample(int sample) = 0;
+};
+
+
+/**
+ * This class reads data from a fake sensor in the background
+ * and calls a callback function whenever data is available.
+ **/
+class FakeSensor : public CppTimer {
+
+public:
+	FakeSensor() {
+	}
+
+	~FakeSensor() {
+		stop();
+	}
+
+	/**
+	 * Sets the callback which is called whenever there is a sample
+	 **/
+	void setCallback(SensorCallback* cb) {
+		sensorCallback = cb;
+	}
+
+	/**
+	 * Starts the data acquisition in the background and the
+	 * callback is called with new samples
+	 **/
+	void startSensor() {
+		start(250000000);
+	}
+
+	/**
+	 * Stops the data acquistion
+	 **/
+	void stopSensor() {
+		stop();
+	}
+
+	/**
+	 * Fake the arrival of data
+	 **/
+	void timerEvent() {
+//		float value = sin(t) * 5 + 20;
+        if (!cap.isOpened()) {
+            std::cout << "Cannot open camera\n";
+        }
+		t += 0.1;
+
+        while (true) {
+            i++;
+            // Extraction of videos
+            bool ret = cap.read(frame); // or cap >> frame;
+            if (!ret) {
+                std::cout << "Can't receive frame (stream end?). Exiting ...\n";
+                break;
+            }
+            if (nullptr != sensorCallback) {
+                sensorCallback->hasSample(frame);
+            }
+            // quit
+            if (cv::waitKey(1) == 'q') {
+                break;
+            }
+        }
+
+
+
+private:
+	SensorCallback* sensorCallback = nullptr;
+	float t = 0;
+    cv::VideoCapture cap;
+    uint8_t id = 0;
+    cv::Mat current_frame;
+    cv::Mat frame;
+    cv::Mat gray;
+    int i=0;
+};
+
+
+#endif
