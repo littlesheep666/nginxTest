0445e0a5776bc7c1446c2a3b734c35675fb31660
XiaoyangWang <2626395W@student.gla.ac.uk>
2022-04-17 15:24:38 +0100

    change frame

--- fake_sensor_demo/fakesensor.h
+++ fake_sensor_demo/fakesensor.h
@@ -67,7 +67,6 @@ public:
         t += 0.1;
         if (nullptr != sensorCallback) {
             sensorCallback->hasSample(value);
-            sensorCallback->hasSampleCV(image);
         }
     }
 
